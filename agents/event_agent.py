588413844e769b2e26140191d91a8a477893856b
ALi <ali280306@gmail.com>
2026-01-17 00:22:29 +0530

    Made the backend running but apis not working

--- agents/event_agent.py
+++ agents/event_agent.py
@@ -2,6 +2,7 @@ from duckduckgo_search import DDGS
 from services.scraper import scrape_page
 from services.llm_extractor import extract_event
 
+
 def discover_events(city: str):
     queries = [
         f"upcoming events in {city}",
@@ -13,21 +14,24 @@ def discover_events(city: str):
     events = []
 
     for q in queries:
-     with DDGS() as ddg:
-        results = ddg(q, max_results=3)
-        for r in results:
-            url = r.get("href")
-            if not url:
-                continue
+        with DDGS() as ddgs:
+            results = ddgs.text(q, max_results=3)
 
-            page_text = scrape_page(url)
-            if len(page_text) < 300:
-                continue
+            for r in results:
+                url = r.get("href")
+                if not url:
+                    continue
 
-            structured = extract_event(page_text)
-            events.append({
-                "source_url": url,
-                "data": structured
-            })
+                page_text = scrape_page(url)
+                if len(page_text) < 300:
+                    continue
+
+                structured = extract_event(page_text)
+
+                events.append({
+                    "query": q,
+                    "source_url": url,
+                    "data": structured
+                })
 
     return events
